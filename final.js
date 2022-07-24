920aed4021158852df0bd3d31c01591c09fd6548
angelinelykk <angelinelyk@berkeley.edu>
2022-07-23 23:24:30 -0400

    push

--- final.js
+++ final.js
@@ -114,3 +114,23 @@ $("#b4").click(function(){
         });
     });
 });
+
+var cursor0 = document.getElementById("cursor0");
+var cursor1 = document.getElementById("cursor1");
+var cursor2 = document.getElementById("cursor2");
+document.onmousemove = function (e) {
+    cursor0.style.left = (e.pageX+30) + "px";
+    cursor0.style.top = (e.pageY+20) + "px";
+    cursor1.style.left = (e.pageX+45) + "px";
+    cursor1.style.top = (e.pageY+20) + "px";
+    cursor2.style.left = (e.pageX+55) + "px";
+    cursor2.style.top = (e.pageY+20) + "px";
+    cursor0.style.display="block";
+    cursor1.style.display="block";
+    cursor2.style.display="block";
+};
+document.onscroll = function () {
+    cursor0.style.display="none";
+    cursor1.style.display="none";
+    cursor2.style.display="none";
+}
